7a60a4f1c58801a929530cc046f19f11926248ed
Mason Garten <garten323@gmail.com>
2022-04-27 00:49:26 -0500

    Saving big changes

--- clock.py
+++ clock.py
@@ -2,22 +2,35 @@ from machine import I2C, Pin
 from urtc import DS3231
 from ssd1306 import SSD1306_I2C
 import utime
+import oled
 
-i2c_rtc = I2C(1,scl = Pin(7),sda = Pin(6),freq = 100000)
-rtc = DS3231(i2c_rtc, address=0x68)
+# i2c_rtc = I2C(1,scl = Pin(7),sda = Pin(6),freq = 100000)
+rtc = DS3231(oled.i2c1, address=0x68)
 utime.sleep_ms(100)
 
 
 def updateTime():
-
-    hour = rtc.datetime().hour
-    minute = rtc.datetime().minute
-    return [hour, minute]
+    year = int(input("Year : "))
+    month = int(input("month (Jan --> 1 , Dec --> 12): "))
+    date = int(input("date : "))
+    day = int(input("day (1 --> monday , 2 --> Tuesday ... 0 --> Sunday): "))
+    hour = int(input("hour (24 Hour format): "))
+    minute = int(input("minute : "))
+    second = int(input("second : "))
+    now = (year,month,date,day,hour,minute,second,0)
+    rtc.datetime(now)
 
 def getTime():
+    list = []
+    hour = "{:02d}".format(rtc.datetime().hour)
+    minute = "{:02d}".format(rtc.datetime().minute)
+    for i in str(hour):
+        list.append(int(i))
+    for i in str(minute):
+        list.append(int(i))
+    return list
 
-
-    return rtc.datetime()
-
-for i in updateTime():
-    print(i)
+t = getTime()
+print(t)
+for i in range(len(t)):
+    oled.writeDigit(t[i], i)
--- oled.py
+++ oled.py
@@ -1,26 +1,60 @@
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import digits
+import utime
 
-i2c0 = I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
-i2c1 = I2C(1,sda=Pin(2), scl=Pin(3), freq=400000)
-oled0 = SSD1306_I2C(128, 64, i2c0)
+
+i2c1 = I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
 oled1 = SSD1306_I2C(128, 64, i2c1)
-oled2 = 
-oled3 = 
+i2c0 = I2C(1 ,sda=Pin(2), scl=Pin(3), freq=400000)
+oled0 = SSD1306_I2C(128, 64, i2c0)
 
-oleds = [oled0, oled1]
+
 scale = 6
+
+def oleds(num):
+      
+    if num == 0:
+        return oled0
+    elif num == 1:
+        return oled0
+    elif num == 2:
+        return oled1
+    elif num == 3:
+        return oled1
+    else:
+        raise
 
 
 def writeDigit(number, oled):
+    display = oleds(oled)
+    pos = 0
+    if oled == 1 or oled == 3:
+        pos = 74
+    
     for y, row in enumerate(digits.digit(number)):
         for x, c in enumerate(row):
-            oleds[oled].pixel(x*scale + 38, y*scale + 4 , c)
-    oleds[oled].show()
+            display.pixel(x*scale + pos, y*scale + 4 , c)
+    display.show()
 
 def invert(oled):
-    oleds[oled].invert(1)
+    display = oleds(oled)
+    pos = 0
+    if oled == 1 or oled == 3:
+        pos = 64
+    for y in range(64):
+        for x in range(64):
+            c = display.pixel(x + pos, y)
+            if c == 0:
+                display.pixel(x + pos, y, 1)
+            elif c == 1:
+                display.pixel(x + pos, y, 0)
+    display.show()
 
-writeDigit(number = 6, oled = 1)
-writeDigit(number = 6, oled = 0)
+def show(list):
+    for i in list:
+        invert(i)
+        utime.sleep_ms(1000)
+        invert(i)
+
+
